305e4e834594f3b1be33e39be61dde268ade0463
IogaMaster <iogamastercode@gmail.com>
2023-09-29 20:01:53 -0600

    Update autocmd's to use neocord name

--- lua/lib/asset.lua
+++ lua/lib/asset.lua
@@ -1,7 +1,7 @@
 local asset = {}
 
 function asset.to_url(asset_name)
-  return string.format("https://raw.githubusercontent.com/IogaMaster/neocord/main/assets/icons/%s.png", asset_to_url)
+    return string.format("https://raw.githubusercontent.com/IogaMaster/neocord/main/assets/icons/%s.png", asset_to_url)
 end
 
 return asset
