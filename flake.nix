387854d81a2c55207b7223ab5221f9d0afc6b273
IogaMaster <iogamastercode@gmail.com>
2024-02-12 16:32:16 -0700

    add package to flake

--- flake.nix
+++ flake.nix
@@ -6,19 +6,31 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            stylua
-            lua53Packages.luacheck
-          ];
 
-          buildInputs = with pkgs; [ ];
+        nativeBuildInputs = with pkgs; [
+          stylua
+          lua53Packages.luacheck
+        ];
+
+        buildInputs = with pkgs; [];
+      in {
+        devShells.default = pkgs.mkShell {inherit nativeBuildInputs buildInputs;};
+
+        packages = with pkgs; rec {
+          default = neocord;
+          neocord = vimUtils.buildVimPlugin {
+            name = "neocord";
+            src = ./.;
+          };
         };
       }
     );
